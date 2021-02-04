18d20123f8898a8d82d6af9706fa3c65d9e33c8a
ChetanKarwa <cbkarwa@gmail.com>
2021-02-04 12:32:39 +0530

    started

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,9 +1,16 @@
-import React from "react";
-import '../styles/App.css';
+import React, {useState} from "react";
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 const App = () => {
+  const [date,setDate] = useState(new Date());
   return (
     <div id="main">
+      <Calendar onChange = {(date) => {setDate(date); console.log(date)}} value = {date}/>
+      <div>
+        
+      </div>
     </div>
+
   )
 }
 
